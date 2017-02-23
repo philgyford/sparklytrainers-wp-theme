0ed09151d3d35f1c37e11f77fba112a091e66f45
Phil Gyford <phil@gyford.com>
2017-02-23 11:21:48 +0000

    Display entry-footer for sparkly_review post types on search pages

---
+++ template-parts/content-search.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template part for displaying results in search pages
+ *
+ * OVERRIDING TWENTY SIXTEEN'S DEFAULT purely to add 'sparkly_review' as a
+ * post type we'll display the footer for.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<?php twentysixteen_excerpt(); ?>
+
+	<?php if ( in_array(get_post_type(), array('post', 'sparkly_review')) ) : ?>
+
+		<footer class="entry-footer">
+			<?php twentysixteen_entry_meta(); ?>
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+
+	<?php else : ?>
+
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<footer class="entry-footer"><span class="edit-link">',
+				'</span></footer><!-- .entry-footer -->'
+			);
+		?>
+
+	<?php endif; ?>
+</article><!-- #post-## -->
+
+
